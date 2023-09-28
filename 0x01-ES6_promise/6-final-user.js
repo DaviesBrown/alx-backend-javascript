ac716809f21ebd7cc3b4d72f96c87a29fea0a159
Davies Brown <nwosudavid77.com>
2023-09-28 03:33:30 +0100

    final user

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,7 +1,19 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstname, lastname, filename) {
-  return Promise.allSettled([signUpUser(firstname, lastname), uploadPhoto(filename)])
-    .then((v) => {return v});
+export default function handleProfileSignup(firstName, lastName, filename) {
+  const signup = signUpUser(firstName, lastName);
+  const upload = uploadPhoto(filename);
+  const promises = [signup, upload];
+  return Promise.allSettled(promises)
+    .then((results) => {
+      results.map((result) => {
+        if (result.status) {
+          console.log({
+            status: result.status,
+            value: result.value || `Error ${result.reason.message}`,
+          });
+        }
+      });
+    });
 }
