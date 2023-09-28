55901abce0ca37291c8ff7bfbaed60548e68d65c
Davies Brown <nwosudavid77.com>
2023-09-28 03:22:35 +0100

    .all

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,10 +1,14 @@
 export default function handleResponseFromAPI(promise) {
-  promise.then(() => ({
-    status: 200,
-    body: 'success',
-  })).catch((err) => {
+  promise.then(() => {
+    return {
+      status: 200,
+      body: 'success',
+    };
+  });
+  promise.catch(() => {
     throw new Error();
-  }).finally(() => {
+  });
+  promise.finally(() => {
     console.log('Got a response from the API');
   });
 }
