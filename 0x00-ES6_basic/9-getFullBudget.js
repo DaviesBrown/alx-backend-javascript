a649e77aef7e889d5ff76c869799d0454e27897c
Davies Brown <nwosudavid77@gmail.com>
2023-10-01 00:30:46 +0100

    es6 basics fix

--- 0x00-ES6_basic/9-getFullBudget.js
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -4,8 +4,8 @@ export default function getFullBudgetObject(income, gdp, capita) {
   const budget = getBudgetObject(income, gdp, capita);
   const fullBudget = {
     ...budget,
-    getIncomeDollars: (income) => `$${income}`,
-    getIncomeEuros: (income) => `${income} euros`,
+    getIncomeInDollars: (income) => `$${income}`,
+    getIncomeInEuros: (income) => `${income} euros`,
   };
 
   return fullBudget;
