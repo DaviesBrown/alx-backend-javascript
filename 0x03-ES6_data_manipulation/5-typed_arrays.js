d952a7235816abd3575d33a7a5bd2d2573a261f7
David Brown <69555259+DaviesBrown@users.noreply.github.com>
2023-10-13 21:51:28 +0100

    Update 5-typed_arrays.js

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,10 +1,8 @@
 export default function createInt8TypedArray(length, position, value) {
-  let buffer = new ArrayBuffer(length);
-  if (position >= length - 1) {
+  if (position >= length) {
     throw new Error('Position outside range');
   }
-  buffer[position] = value;
-  buffer = new DataView(buffer);
-
-  return buffer;
+  const buf = new DataView(new ArrayBuffer(length), 0, length);
+  buf.setInt8(position, value);
+  return buf;
 }
