afc96f6a438e058e692e78e8da47b364a2a9dd59
Davies Brown <nwosudavid77@gmail.com>
2023-11-25 17:31:53 +0100

    integration test

--- 0x06-unittests_in_js/10-api/api.test.js
+++ 0x06-unittests_in_js/10-api/api.test.js
@@ -7,6 +7,7 @@ describe("GET /", function () {
     const url = "http://localhost:7865"
     it("should return index page", function() {
         request(url, (err, req, body) => {
+            if (err) done(err);
             expect(req.statusCode).to.equal(200);
             expect(body).to.equal("Welcome to the payment system");
         });
@@ -27,4 +28,56 @@ describe('GET /cart/:id', () => {
             expect(req.statusCode).to.equal(404);
         });
     });
-  });
+});
+
+describe("POST /login", function() {
+    it("should return a welcome message with the username", function(done) {
+        const options = {
+            url: "http://localhost:7865/login",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify({userName: "Betty"})
+        };
+        request.post(options, (err, res, body) => {
+            expect(res.statusCode).to.equal(200);
+            expect(body).to.equal("Welcome Betty");
+            done();
+        });
+    });
+    it("should return 400 error when no username is passed", function(done) {
+        const options = {
+            url: "http://localhost:7865/login",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify({userName: ""})
+        };
+        request.post(options, (err, res, body) => {
+            expect(res.statusCode).to.equal(200);
+            done();
+        });
+    });
+    it("should return 400 error when no body is passed", function(done) {
+        const options = {
+            url: "http://localhost:7865/login",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify({})
+        };
+        request.post(options, (err, res, body) => {
+            expect(res.statusCode).to.equal(200);
+            done();
+        });
+    });
+});
+
+describe('GET /available_payments', () => {
+    it('should return the available payment methods', (done) => {
+      request.get('http://localhost:7865/available_payments', (error, response, body) => {
+        expect(response.statusCode).to.equal(200);
+        expect(JSON.parse(body)).to.deep.equal({
+            payment_methods: {
+              credit_cards: true,
+              paypal: false
+            }
+        });
+        done();
+      });
+    });
+});
